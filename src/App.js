739732973d201b8460b819e46eeb28d3960e0689
Vitaliy <43200383+s04v@users.noreply.github.com>
2022-08-14 02:04:18 +0200

    Implement the Build button and repsonse data output

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import "ace-builds/src-noconflict/theme-github";
 import "ace-builds/src-noconflict/ext-language_tools";
 import {useState} from "react";
 import CircularProgress from '@mui/material/CircularProgress';
+import { retrieveFollowingIdentifier } from 'ace-builds/src-noconflict/ext-language_tools';
 
 const defaultCode = `func main(): void {
     var text: string = "Hello, world!";
@@ -19,27 +20,50 @@ const App = () => {
     const [code, setCode] = useState(defaultCode);
     const [building, setBuilding] = useState(false);
     const [terminal, setTerminal] = useState("");
-
+    const [asm, setAsm] = useState("");
     const build = () => {
+        
+
         setTerminal("Start building...");
         setBuilding(true);
         setTimeout(() => {
-            setBuilding(false);
-            setTerminal('Success build');
-        }, 500)
+            fetch('http://compiler-playground-backend.norwayeast.cloudapp.azure.com:3000/build',{
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'text/plain'
+                },
+                body: code
+            })
+            .then((response) => response.json())
+            .then((res) => {
+                setBuilding(false);
+                setAsm("");
+                console.log(res);
+                if(res.status === 400){ 
+                    setTerminal(res.msg);
+                } else {
+                    setTerminal(res.output);
+                    setAsm(res.asm);
+                }
+            });       
+        }, 500);
+        
     }
 
-    const buildAndRun = () => {
-
-    }
     return (
     <div className="app">
         <div className="buttons">
             <button onClick={build}>Build</button>
-            <button onClick={buildAndRun}>Build and Run</button>
         </div>
 
         <div className="editors">
+            { building ?
+                <div className="loader">
+                    <CircularProgress />
+                </div>
+                :
+                <></>
+            }
             <AceEditor
                 value={code}
                 onChange={(newCode) => setCode(newCode)}
@@ -52,15 +76,10 @@ const App = () => {
                 editorProps={{ $blockScrolling: true }}
             />
             <div>
-                { building ?
-                    <div className="loader">
-                        <CircularProgress />
-                    </div>
-                    :
-                     <></>
-                }
+
 
                 <AceEditor
+                    value={asm}
                     mode="assembly_x86"
                     theme="github"
                     width='40vw'
@@ -76,7 +95,7 @@ const App = () => {
 
         </div>
         <div className="output">
-            {terminal.split('\n').map(str => <p>> {str}</p>)}
+            {terminal}
         </div>
 
     </div>
